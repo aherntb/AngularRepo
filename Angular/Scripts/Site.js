3d3b592fd937f89bc345455a7ca9cd7a5f1facd0
Brian Ahern <bahern@decaresystems.ie>
2019-03-07 10:39:25 +0000

    Initial Commit

---
+++ Angular/Scripts/Site.js
@@ -0,0 +1,36 @@
+﻿
+var app = angular.module("testModule", []);
+
+var MainController = function ($scope, $http) {
+
+    var onUserComplete = function (response) {
+        $scope.user = response.data;
+
+        $http.get($scope.user.repos_url).then(onRepos, onError);
+    };
+
+    var onError = function (reason) {
+        $scope.error = "Could not fetch the data";
+    };
+
+    var onRepos = function (response) {
+
+        $scope.repos = response.data;
+
+    };
+
+    $scope.search = function (user) {
+        $http.get("https://api.github.com/users/" + user)
+            .then(onUserComplete, onError);
+    }
+
+
+
+    $scope.message = "Hello Angular";
+    $scope.userName = "Angular";
+    $scope.repoSortOrder = "-stargazers_count";
+
+
+};
+
+app.controller("MainController", ["$scope", "$http", MainController]);
